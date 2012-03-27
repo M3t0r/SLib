ba1870e38e4cf6a82b03177652a108a960f8e653
M3t0r <metor1@gmail.com>
2012-03-27 19:26:12 +0200

    trashed old files

--- slib/ajax.js
+++
@@ -1,8 +0,0 @@
-SLib.newHTTPRequest = function() {
-	if(SLib.browser.ie && SLib.browser.ie.major < 7) // in ie6
-		client = new ActiveXObject("Microsoft.XMLHTTP");
-	else // in ff, safari, opera und ie ab v7
-		client = new XMLHttpRequest();
-
-	return client;
-};
